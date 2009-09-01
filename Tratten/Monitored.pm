d82b4e1ee449a5a2b61f9383e922ceffeaeb3e27
kattla <kattlachan@gmail.com>
2009-09-01 16:58:48 +0200

    Factor out some code into a separate module.
    Code for retrival of changedetection data where previously present in
    both 'trattify' and 'get-monitored'. Now it resides in it's own module
    instead (Tratten/Monitored.pm).

---
+++ Tratten/Monitored.pm
@@ -0,0 +1,36 @@
+package Tratten::Monitored;
+
+use strict;
+use warnings;
+use base 'Exporter';
+our @EXPORT = qw(get_monitored);
+
+sub get_monitored {
+  open my $F, "changedetection.account" or die;
+
+  chomp(my $email = <$F>);
+  chomp(my $password = <$F>);
+  die("Need email & password each on its own line, in the changedetection.account file. Quitting.") unless $email and $password;
+
+  my $args = "-# --cookie cookies --cookie-jar cookies";
+  my $form = "-F 'email=$email' -F 'frompage=http://www.changedetection.com/monitors.html' -F 'login=log in' -F 'op=login' -F 'pw=$password'";
+
+  `curl $args --url http://www.changedetection.com/index.html`;
+  $_ = `curl $args $form -L --url http://www.changedetection.com/login.html`;
+
+  my %ret;
+
+  do {
+    my @matches = /<a href="\/log\/[^"]+" title="[^"]+"/g;
+    for (@matches) {
+      /href="([^"]+)" title="([^ ]+)\s+([^"]+)/ or die;
+      print STDERR "Warning: Reference number $2 is not unique!\n" if $ret{$2};
+      $ret{$2} = ["http://www.changedetection.com$1", $3];
+    }
+  } while (/<a href='(\/monitors.html\?rclstart=\d+)'>next<\/a>/
+           and ($_ = `curl $args --url http://www.changedetection.com$1`, 1));
+
+  return \%ret;
+}
+
+1;
