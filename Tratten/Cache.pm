cbf22cca5c4538e6066406558157108d45d8f5e7
kattla <kattla42@gmail.com>
2010-11-23 20:36:38 +0100

    Use cache.

---
+++ Tratten/Cache.pm
@@ -0,0 +1,38 @@
+package Tratten::Cache;
+
+use strict;
+use warnings;
+use Tratten::Throttle;
+use CHI;
+use Digest::SHA;
+use LWP::UserAgent;
+
+system "mkdir -p _cache";
+
+our $cache = CHI->new( driver => 'File', root_dir => '_cache' );
+
+sub get { $cache->get(@_) }
+sub set { $cache->set(@_) }
+
+sub URI {
+  my $uri = shift;
+  my %arg = @_;
+  my $data = $cache->get($uri);
+  return $data->{content} if $data && not ($arg{expire_if} && $arg{expire_if}($data));
+  my $ua = LWP::UserAgent->new;
+  Tratten::Throttle::sync($uri);
+  my $r = $ua->get($uri);
+  die $r->status_line unless $r->is_success;
+  $data = {
+    uri => $uri,
+    headers => $r->headers_as_string,
+    timestamp => time,
+    sha256 => Digest::SHA::sha256_hex($r->decoded_content),
+    content => $r->decoded_content,
+  };
+  if ($arg{meta}) { $data->{$_} = $arg{meta}->{$_} for keys %{$arg{meta}} }
+  $cache->set($uri, $data);
+  return $data->{content};
+}
+
+1;
--- Tratten/Monitored.pm
+++ Tratten/Monitored.pm
@@ -2,35 +2,65 @@ package Tratten::Monitored;
 
 use strict;
 use warnings;
-use Tratten::Throttle;
-use base 'Exporter';
-our @EXPORT = qw(get_monitored);
+use Tratten::Cache;
 
-sub get_monitored {
+our %uri;
+our %refnum;
+
+my $email;
+my $password;
+
+{
+  &load_account_info;
+  my $key = "monitors:$email";
+  my $cached = &Tratten::Cache::get($key);
+  if ($cached) {
+    print STDERR "Loading changedetection data from cache...\n";
+    %uri = %{ $cached };
+    for (keys %uri) { $refnum{$uri{$_}->{refnum}} = $uri{$_} if $uri{$_}->{refnum} }
+  } else {
+    print STDERR "Logging in to changedetection...\n";
+    &fetch_monitors;
+    &Tratten::Cache::set($key, \%uri, "1 day");
+  }
+  my $number_monitored = keys %uri;
+  print STDERR "$number_monitored pages are monitored.\n";
+}
+
+sub load_account_info {
   my $filename = "changedetection.account";
   open my $F, $filename or die "Could not open file '$filename': $!";
 
-  chomp(my $email = <$F>);
-  chomp(my $password = <$F>);
+  chomp($email = <$F>);
+  chomp($password = <$F>);
   die("Need email & password each on its own line, in the changedetection.account file. Quitting.") unless $email and $password;
+}
 
+sub fetch_monitors {
   my $args = "-# --sslv3 --cookie-jar cookies";
   my $form = "-F 'email=$email' -F 'frompage=http://www.changedetection.com/monitors.html' -F 'login=log in' -F 'op=login' -F 'pw=$password'";
 
   `curl $args --url https://www.changedetection.com/index.html`;
   $_ = `curl $args --cookie cookies $form -L --url https://www.changedetection.com/login.html`;
 
-  my %ret;
-
   do {
-    while (/<a href="(\/log\/[^"]+)" title="([^ ]+)\s+([^"]+)".+?href="([^"]+)"/g) {
-      print STDERR "Warning: Reference number $2 is not unique!\n" if $ret{$2};
-      $ret{$2} = ["http://www.changedetection.com$1", $3, $4];
+    while (/<input\s.+?<td\s.+?>(.*?)<\/td>.+?<a href="(\/log\/[^"]+)" title="([^"]+)".+?href="([^"]+)"/g) {
+      my %x = (
+        log => "http://www.changedetection.com$2",
+        name => $3,
+        uri => $4,
+        last_notified => $1,
+      );
+      if ($x{name} =~ /^([^ ]+)\s+(.+)$/) {
+        $x{refnum} = $1; $x{desc} = $2;
+        print STDERR "WARNING: Reference number $x{refnum} is not unique!\n" if $refnum{$x{refnum}};
+        $refnum{$x{refnum}} = \%x;
+      }
+      print STDERR "WARNING: Monitored URI is not unique: $x{uri}!\n" if $uri{$x{uri}};
+      $uri{$x{uri}} = \%x;
     }
   } while (/<a href='(\/monitors\.html\?rclstart=\d+)'>next<\/a>/
            and ($_ = `curl $args --cookie cookies --url https://www.changedetection.com$1`, 1));
-
-  return \%ret;
 }
 
 1;
